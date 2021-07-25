dd76498377afea359bd98ee88d45b1c8d43c0ecd
abdooo9 <76847516+abdooo9@users.noreply.github.com>
2021-07-25 14:48:00 +0300

    Fix Error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.search = async function (arg) {
     if (!arg) {
         throw Error("None arg for search");
     } else {
-        let url = `https://pypi.org/pypi/${arg.toLowerCase()}/json`
+        let url = `https://pypi.org/pypi/${encodeURIComponent(arg.toLowerCase())}/json`
         let data = await fetch(url).then(res => res.json());
         if (data.error) {
             throw Error("Can't find this package in npm");

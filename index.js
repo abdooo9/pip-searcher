83442eda571d7e3407e053f907b712d2bc9f6ec3
abdooo9 <76847516+abdooo9@users.noreply.github.com>
2021-07-30 20:51:15 +0300

    Fix Bug

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch')
-
+require('colors')
 exports.search = async function (arg) {
     if (!arg) {
-        throw Error("None arg for search");
+        throw Error("None arg for search".red);
     } else {
         let url = `https://pypi.org/pypi/${encodeURIComponent(arg.toLowerCase())}/json`
         let data = await fetch(url).then(res => res.json());
-        if (data.error) {
-            throw Error("Can't find this package in npm");
+        if (!data) {
+            throw Error("Can't find this package in PyPI");
         } else {
             return data;
         }
